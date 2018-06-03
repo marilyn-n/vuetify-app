91d6a4936db77605e36b13b40de8abaeab37fadd
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-06-03 11:48:04 -0500

    Home, Toolbar and Footer components created

---
+++ src/components/AppToolBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <v-toolbar color="grey darken-4" dark="">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">Vuetify</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>About</v-btn>
+      <v-btn flat>Contact</v-btn>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="hello">
-    <v-toolbar color="grey darken-4" dark="">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title class="white--text">Vuetify</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>About</v-btn>
-      <v-btn flat>Contact</v-btn>
-      <v-btn flat>Login</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
-  <div>
+<div class="app-home">
+  <App-toolBar></App-toolBar>
+   <div>
     <v-layout column class="main">
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
@@ -19,13 +10,11 @@
             <v-flex
               v-for="card in cards"
               v-bind="{ [`xs${card.flex}`]: true }"
-              :key="card.title"
-            >
+              :key="card.title">
               <v-card>
                 <v-card-media
                   :src="card.src"
-                  height="200px"
-                >
+                  height="200px">
                   <v-container fill-height fluid>
                     <v-layout fill-height>
                       <v-flex xs12 align-end flexbox>
@@ -53,40 +42,31 @@
       </v-card>
     </v-flex>
   </v-layout>
+    <FooterComponent></FooterComponent>
   </div>
-  <v-footer height="auto" class="grey darken-3">
-    <v-layout row wrap justify-center>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        flat
-      >
-        {{ link }}
-      </v-btn>
-      <v-flex xs12 py-3 text-xs-center white--text>
-        &copy;2018 — <strong>Vuetify</strong>
-      </v-flex>
-    </v-layout>
-  </v-footer>
-  </div>
+</div>
 </template>
 
 <script>
+
+import FooterComponent from './FooterComponent';
+import AppToolBar from './AppToolBar';
+
 export default {
+  components: {
+    AppToolBar,
+    FooterComponent,
+  },
   data: () => ({
-    name: 'HelloWorld',
     cards: [
       { title: 'Pre-fab homes', src: 'https://richmondbizsense.com/wp-content/uploads/2015/10/Parade-of-Homes-2.jpg', flex: 12 },
       { title: 'Favorite road trips', src: 'http://babiestravellite.com/wp-content/uploads/2015/08/summer-road-trip.jpg', flex: 6 },
       { title: 'Best airlines', src: 'https://skift.com/wp-content/uploads/2017/07/allegiant-airlines-.jpg', flex: 6 },
     ],
-    links: ['Home', 'About Us', 'Team', 'Services', 'Blog', 'Contact Us'],
   }),
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
